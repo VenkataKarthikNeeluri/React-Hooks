9d044d5c0320b9a7db6dd460c67e7a62d5a12580
VenkataKarthikNeeluri <nvenkatakarthik028@gmail.com>
2023-06-10 23:16:29 +0530

    updated useCallback,useMemo,useRef hooks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Ex2 from "./components/Ex2";
 import Ex3 from "./components/Ex3";
 import Ex4 from "./components/Ex4";
 import Pnf from "./components/Pnf";
+import Ex5 from "./components/Ex5";
+import Ex6 from "./components/Ex6";
 
 function App(){
   return(
@@ -14,10 +16,11 @@ function App(){
     <Routes>
       <Route path={`/`} element={<Ex1/>}/>
       <Route path={`/ex1`} element={<Ex1/>}/>
-
       <Route path={`/ex2`} element={<Ex2 itemsPerPage={10}/>}/>
       <Route path={`/ex3`} element={<Ex3 itemsPerPage={5}/>}/>
       <Route path={`/ex4`} element={<Ex4/>}/>
+      <Route path={`/ex5`} element={<Ex5/>}/>
+      <Route path={`/ex6`} element={<Ex6/>}/>
       <Route path={`/*`} element={<Pnf/>}/>
 
     </Routes>
---
+++ src/components/Ex6.js
@@ -0,0 +1,46 @@
+import React,{useRef} from 'react'
+
+function Ex6(props) {
+    const fUser = useRef()
+    const fPass = useRef()
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const data = {
+            user: fUser.current.value,
+            pass: fPass.current.value 
+        };
+        console.log('new user =', data) 
+    }
+
+  return (
+    <div className='container'>
+        <div className="row">
+            <div className="col-md-12 text-center">
+                <h3 className="display-3 text-success">useRef</h3>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-md-6 offset-md-3">
+                <form autoComplete="off" onSubmit={submitHandler}>
+                    <div className="form-group mt-2">
+                        <label htmlFor="user">Username</label>
+                        <input type="text" name="user" id="user" ref={fUser} className='form-control' required />
+                    </div>
+                    <div className="form-group mt-2">
+                        <label htmlFor="user">Password</label>
+                        <input type="password" name="pass" id="pass" ref={fPass} className='form-control' required />
+                    </div>
+                    <div className="form-group mt-2">
+                        <input type="submit" name="" id="" value="Login" className='btn btn-success'/>
+                    </div>
+                </form>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Ex6
